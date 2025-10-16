22e57fcdf0e7dbf3d4c575677b96a7de19241f09
Umer <janumar2900@gmail.com>
2025-10-16 20:24:23 +0530

    DONE

--- Main/app.py
+++ Main/app.py
@@ -2,13 +2,13 @@ import sys
 import os
 
 # This allows imports from other modules in the same directory
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '.')))
+# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '.')))
 
 
 import streamlit as st
 from PyPDF2 import PdfReader
-# from Gemini_services import get_gemini_response, input_pdf_text
-from Gemini_services.gemini_services import get_gemini_response, input_pdf_text
+from Gemini_services import get_gemini_response, input_pdf_text
+# from Gemini_services.gemini_services import get_gemini_response, input_pdf_text
 from auth import login_user, signup_user
 from database import supabase_client
 
